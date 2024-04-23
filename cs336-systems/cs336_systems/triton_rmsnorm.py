95ab928feae2b5ab5c2a5afd8e1eac0c0a001395
go-akshit <akshit@stanford.edu>
2024-04-22 20:14:55 -0700

    triton_fwd_pass Complete

--- cs336-systems/cs336_systems/triton_rmsnorm.py
+++ cs336-systems/cs336_systems/triton_rmsnorm.py
@@ -27,14 +27,24 @@ def rms_triton(x_ptr : tl.pointer_type,
     offsets = tl.arange(0, BLOCK_SIZE)
     x_ptrs = row_start_ptr + offsets
     weight_ptrs = weight_ptr + offsets
-    output_ptrs = output_ptr + offsets
+    output_ptrs = output_ptr + row_idx*x_row_stride + offsets
 
     mask = offsets < D_MODEL
     row = tl.load(x_ptrs, mask=mask, other=0)
     weight = tl.load(weight_ptrs, mask=mask, other=0)
 
     rms = tl.sqrt(tl.sum(row*row)/D_MODEL)
-    output = row/rms*weight
+    #rms = tl.sum(row*row)/D_MODEL
+    #row_rms = row/rms
+
+    output = (row*weight)/rms
+
+    #if(row_idx == 0):
+    #    print("d_model", D_MODEL)
+    #    print("rms",rms)
+    #    print("row", row)
+    #    print("rms_rms", row_rms)
+    #    print("output", output)
     tl.store(output_ptrs, output, mask=mask)
 
 
@@ -43,6 +53,7 @@ class rms_norm_triton(torch.autograd.Function):
     def forward(ctx, x, weight):
         ctx.save_for_backward(x, weight)
         d_model = x.shape[-1]
+        orig_shape = x.shape
         x = x.reshape(-1, d_model)
 
         assert len(weight.shape) == 1 and weight.shape[0] == d_model, "Dimension mismatch"
@@ -53,6 +64,7 @@ class rms_norm_triton(torch.autograd.Function):
         y = torch.empty(x.shape, device = x.device)
         n_rows = x.shape[0]
         rms_triton[(n_rows, )](x, weight, x.stride(0), y, d_model, num_warps=16, BLOCK_SIZE=ctx.BLOCK_SIZE)
+        y = y.reshape(orig_shape)
         return y
     
     @staticmethod
