1bd6c0ff34b031abb209896cae39e456652b4389
go-akshit <akshit@stanford.edu>
2024-04-28 13:03:17 -0700

    3.1

--- cs336-systems/cs336_systems/distributed_comm.py
+++ cs336-systems/cs336_systems/distributed_comm.py
@@ -13,7 +13,7 @@ def get_args():
     parser.add_argument("--device", type=str, choices=['cpu', 'gpu'], default='cpu', help="default:cpu")
     parser.add_argument("--data_size", type=str, choices=['512K', '1M', '10M', '50M', '100M', '500M', '1G'], default='512K', help="default:512K")
     parser.add_argument("--n_procs", type=int, choices=[2, 4, 6], default=2, help="default:2")
-    parser.add_argument("--num_warmup_steps", type=int, defualt=5, help="default:5")
+    parser.add_argument("--num_warmup_steps", type=int, default=5, help="default:5")
     parser.add_argument("--num_trial_steps", type=int, default=5, help="default:5")
     args = parser.parse_args()
     return args
@@ -39,22 +39,22 @@ def distributed_demo(rank, *args):
     data_size, backend, device, n_procs, warmup, trial = args
     setup(rank=rank, world_size=n_procs, backend=backend, device=device)
     num_elements = size_to_bytes(data_size)//4
-    data = torch.randint(0, 10, (num_elements,))
+    tensor_data = torch.randint(0, 10, (num_elements,))
 
-    for _ in warmup:
-        print(f"rank: {rank} data (before all-reduce): {data}")
-        dist.all_reduce(data, async_op=False)
-        print(f"rank: {rank} data (after all-reduce): {data}")
+    for _ in range(warmup):
+        data = tensor_data.clone()
+        dist.all_reduce(tensor=data, async_op=False)
         if(device == 'gpu'):
             torch.cuda.synchronize()
         dist.barrier()
     
     durations = []
-    for _ in trial:
+    for _ in range(trial):
+        data = tensor_data.clone()
         print(f"rank: {rank} data (before all-reduce): {data}")
         start_time = timeit.default_timer()
         
-        dist.all_reduce(data, async_op=False)
+        dist.all_reduce(tensor=data, async_op=False)
         
         if(device == 'gpu'):
             torch.cuda.synchronize()
@@ -65,7 +65,7 @@ def distributed_demo(rank, *args):
         durations.append(duration)
         print(f"rank: {rank} data (after all-reduce): {data}")
     
-    print(f"Mean time = {np.mean(durations)}. std deviation = {np.std(durations)}")
+    print(f"Mean time = {np.mean(durations):0.6f}. std deviation = {np.std(durations):0.6f}")
         
 
 if __name__ == "__main__":
@@ -75,5 +75,5 @@ if __name__ == "__main__":
                                                                         args.device, 
                                                                         args.n_procs, 
                                                                         args.num_warmup_steps, 
-                                                                        args.num_trial_steps) 
+                                                                        args.num_trial_steps), 
                                                                         )
