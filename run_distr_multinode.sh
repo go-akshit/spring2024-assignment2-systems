e65db2563339a91233dc58f3a6117e40ba2c4ea3
akshit317 <akshitgoel4@gmail.com>
2024-04-28 21:12:20 -0700

    3.1_multinode

--- run_distr_multinode.sh
+++ run_distr_multinode.sh
@@ -2,9 +2,13 @@
 #SBATCH --job-name=distr_multi
 #SBATCH --cpus-per-task=2
 #SBATCH --ntasks-per-node=2
+#SBATCH --gpus-per-node=2
 #SBATCH --nodes=2
 #SBATCH --mem=8G
-#SBATCH --time=00:02:00
+#SBATCH --time=00:05:00
+#SBATCH --partition=a2
+#SBATCH --output=distr_multi_%j.out
+#SBATCH --error=distr_multi_%j.err
 
 eval "$(conda shell.bash hook)"
 conda activate cs336_systems
@@ -14,4 +18,10 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 echo "MASTER_PORT: ${MASTER_PORT}"
 echo "MASTER_ADDR: ${MASTER_ADDR}"
 
-srun python3 cs336-systems/cs336_systems/distributed_multinode.py --num_warmup_steps 3 --num_trial_steps 1
+srun python3 cs336-systems/cs336_systems/distributed_multinode.py
+
+mkdir -p logs/output
+mkdir -p logs/error
+
+mv distr*out logs/output/.
+mv distr*err logs/error/.
