65ada9dc2a8fa267d82dca00b8280a2711335a0c
go-akshit <akshit@stanford.edu>
2024-04-30 23:03:36 -0700

    naive ddp

--- cs336-systems/cs336_systems/naive_ddp.py
+++ cs336-systems/cs336_systems/naive_ddp.py
@@ -7,7 +7,7 @@ import argparse
 import timeit
 import numpy as np
 from datetime import timedelta
-
+from copy import deepcopy
 import cs336_basics.model as model_def
 from cs336_basics.data import get_batch
 import cs336_basics.nn_utils as utils
@@ -22,11 +22,11 @@ def get_args():
     parser.add_argument("--num_trial_steps", type=int, default=1, help="default:1")
     parser.add_argument("--context_length", type=int, default=128, help="context length for the model, default=128")
     parser.add_argument("--vocab_size", type=int, default=10000, help="vocab size for the model, default=10000")
-    parser.add_argument("--batch_size", type=int, default=16, help="Batch size for the model, default=16")
-    parser.add_argument("--d_model", type=int, default=768, help="d_model for the model, default=small i.e. 768")
-    parser.add_argument("--d_ff", type=int, default=3072, help="d_ff for the model, default=small i.e. 3072")
-    parser.add_argument("--num_layers", type=int, default=12, help="num_layers for the model, default=small i.e. 12")
-    parser.add_argument("--num_heads", type=int, default=12, help="num_heads for the model, default=small i.e. 12")
+    parser.add_argument("--batch_size", type=int, default=4, help="Batch size for the model, default=16")
+    parser.add_argument("--d_model", type=int, default=12, help="d_model for the model, default=small i.e. 768")
+    parser.add_argument("--d_ff", type=int, default=12, help="d_ff for the model, default=small i.e. 3072")
+    parser.add_argument("--num_layers", type=int, default=2, help="num_layers for the model, default=small i.e. 12")
+    parser.add_argument("--num_heads", type=int, default=2, help="num_heads for the model, default=small i.e. 12")
     parser.add_argument("--profiler", action="store_true", default=False, help="If want to use pytorch profiler, default= false")
     parser.add_argument("--norm_layer", type=str, default='rms', choices=['rms', 'ln'], help="type of norm layer")
     args = parser.parse_args()
@@ -53,10 +53,10 @@ def setup(backend, device):
     return rank, local_rank, world_size, local_world_size
 
 
-def run(args, model, optimizer, num_iterations, input, target):
+def run(args, model, optimizer, num_iterations, inputs, target):
     for _ in range(num_iterations):
         optimizer.zero_grad()
-        x = input
+        x = inputs
         logits = model(x)
         loss = utils.cross_entropy(logits, target)
         loss.backward()
@@ -69,16 +69,16 @@ def run(args, model, optimizer, num_iterations, input, target):
         
         optimizer.step()
 
-def run_individual(args, model, optimizer, num_iterations, input, target):
+def run_individual(args, model, optimizer, num_iterations, inputs, target):
     for _ in range(num_iterations):
         optimizer.zero_grad()
-        x = input
+        x = inputs
         logits = model(x)
         loss = utils.cross_entropy(logits, target)
         loss.backward()        
         optimizer.step()
 
-def get_model_optimizer_input_targer(args, device):
+def get_model_optimizer_input_target(args, device):
         
     tokens = np.random.randint(0, 100, 5*args.context_length)
     input, target = get_batch(tokens, args.batch_size, args.context_length, device=device)
@@ -93,39 +93,57 @@ def get_model_optimizer_input_targer(args, device):
                                 residual_pdrop=None, 
                                 norm_layer=args.norm_layer).to(device)
     
-    adam = AdamW(model.parameters())
-    return input, target, model, adam
+    return input, target, model
 
 def broadcast_parameters(model, optimizer):
     for param in model.parameters():
         dist.broadcast(param.data, src=0)
     
-    for state in optimizer.state.values():
-        for k, v in state.items():
-            dist.broadcast(v, src=0)
+    #for state in optimizer.state.values():
+    #    for k, v in state.items():
+    #        dist.broadcast(v, src=0)
 
 def ddp(args):
     #import pdb; pdb.set_trace()
     rank, local_rank, world_size, local_world_size = setup(args.backend, args.device)
     if args.device == 'gpu':
         device = f'cuda:{local_rank}'
-    input, target, model, adam = get_model_optimizer_input_targer(args, device)
+    else:
+        device = 'cpu'
+    input_a, target, model = get_model_optimizer_input_target(args, device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     
+    non_parallel_model = deepcopy(model)
+    non_parallel_optimizer = torch.optim.SGD(non_parallel_model.parameters(), lr=0.1)
     sharded_batch_size = args.batch_size/world_size
-    start_index = rank * sharded_batch_size
-    end_index = start_index + sharded_batch_size
-    sharded_input = input[start_index: end_index]
+    print(args.batch_size)
+    print(sharded_batch_size)
+    start_index = int(rank * sharded_batch_size)
+    end_index = int(start_index + sharded_batch_size)
+    sharded_input = input_a[start_index: end_index]
     sharded_target = target[start_index: end_index]
     
     if(args.device == 'gpu'):
         sharded_input = sharded_input.to(device)
         model = model.to(device)
-        optimizer = optimizer.to(device)
     
     broadcast_parameters(model, optimizer)
-    run(model=model, optimizer=optimizer, num_iterations=1, input=sharded_input, target=sharded_target)
-    run_individual(model=model, optimizer=optimizer, num_iterations=1, input=input, target=target)
+    run(args, model=model, optimizer=optimizer, num_iterations=1, inputs = sharded_input, target=sharded_target)
+    run_individual(args, model=non_parallel_model, optimizer=non_parallel_optimizer, num_iterations=1, inputs=input_a, target=target)
     print("both done")
+    if rank == 0:
+        for non_parallel_model_parameter, ddp_model_parameter in zip(non_parallel_model.parameters(), model.parameters()):
+            
+            print(non_parallel_model_parameter)
+            print(ddp_model_parameter)
+            if(non_parallel_model_parameter.requires_grad and ddp_model_parameter.requires_grad):
+                # The only parameters that change are those that require_grad
+                assert not torch.allclose(non_parallel_model_parameter, ddp_model_parameter)
+            else:
+                # parameters that don't require_grad shouldn't change
+                assert torch.allclose(non_parallel_model_parameter, ddp_model_parameter)
+
+            print('assertion_passed')
 
 
 def main():
